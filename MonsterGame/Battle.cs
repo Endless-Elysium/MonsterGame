a1001c501d34b2376b60bd20f489ed0f6db5c736
Ryn <124733337+Endless-Elysium@users.noreply.github.com>
2023-08-09 18:09:38 -0500

    e

--- MonsterGame/Battle.cs
+++ MonsterGame/Battle.cs
@@ -12,59 +12,37 @@ namespace MonsterGame
         int EXP = 0;
         public void BeginBattle(int AreaType)
         {
-            Foe CurrentFoe = GetFoe(AreaType, BattleRNG.Next(0,3));
-            int FoeSPE = CurrentFoe.SPE;
-            Console.WriteLine(CurrentFoe.Name + " appears");
-            while (CurrentFoe != null && CurrentFoe.HP > 0)
-            {
-                int FoeATK = CurrentFoe.ATK;
-                int Input = UI.DrawBattle();
-                if (FoeSPE > Monsters.PlayerSPE)
-                {
-                    FoeATK -= Monsters.PlayerDEF;
-                    if (FoeATK < 0) FoeATK = 1;
-                    if (Input == 2) FoeATK = 0;
-                    Console.WriteLine("Foe " + CurrentFoe.Name + " attacked for " + (FoeATK) + " amount of damage");
-                }
-                if (Input == 1)
-                {
-                    Console.WriteLine("Your monster attacked " + CurrentFoe.Name + "for " + Monsters.PlayerATK + " amount of damage!!");
-                    CurrentFoe.HP -= Monsters.PlayerATK;
-                    FoeSPE = CurrentFoe.SPE;
-                    if (CurrentFoe.HP <= 0)
-                    {
-                        break;
-                    }
-                }
-                if (FoeSPE < Monsters.PlayerSPE)
-                {
-                    FoeATK -= Monsters.PlayerDEF;
-                    if (FoeATK < 0) FoeATK = 1;
-                    if (Input == 2) FoeATK = 0;
-                    Console.WriteLine("Foe " + CurrentFoe.Name + " attacked for " + (FoeATK) + " amount of damage");
-                }
-                if (Input == 2)
-                {
-                    Console.WriteLine("Your monster defended!");
-                    Console.WriteLine("The foe became slower!");
-                    FoeSPE = 0;
-                }
-            }
-            if (Monsters.PlayerHP > 0)
-            {
-                Console.WriteLine("You win!");
-                Console.WriteLine("Your monster gained " + CurrentFoe.EXP + "EXP");
-                EXP += CurrentFoe.EXP;
-                LevelUP();
-            }
-            void LevelUP()
-            {
-                if (EXP < 100)
-                {
-                    return;
-                }
-                Console.WriteLine("Your monster leveled up!!");
-            }
+           
         }
+        static readonly private List<Foe> Area1FoeList = new List<Foe>
+        {
+            new Foe
+            {
+                Name = "Test 1", // Not working
+                ID = 0,
+                HP = 20,
+                ATK = 1,
+                SPE = 3,
+                EXP = 2,
+            },
+            new Foe
+            {
+                Name = "Test 2",
+                ID = 1,
+                HP = 40,
+                ATK = 1,
+                SPE = 3,
+                EXP = 50,
+            },
+            new Foe
+            {
+                Name = "Test 3",
+                ID = 3,
+                HP = 90,
+                ATK = 1,
+                SPE = 3,
+                EXP = 100,
+            },
+        };
     }
 }
